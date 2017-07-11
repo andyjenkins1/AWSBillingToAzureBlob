d8c1c3bfe3fb10867e1644e22f98ad0a8e048abb
nick Hill <nick-9979@outlook.com>
2017-07-11 18:45:59 +0100

    change assemblyinfo

--- AWSBillingToAzureBlob/Properties/AssemblyInfo.cs
+++ AWSBillingToAzureBlob/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AWSBillingWebJob")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("None")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
